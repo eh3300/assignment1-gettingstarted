013099a61398281f5d39d3aed5aee31493bdf6fe
eh3300 <151074955+eh3300@users.noreply.github.com>
2024-02-02 17:38:37 -0500

    Update gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -14,16 +14,16 @@ def welcome_assignment_answers(question):
     elif question == "Is it possible to decode a message without a key? - Yes/No":
         answer = "No"
     elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
-        answer = "No"
+        answer = "Yes"
     elif question == "What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
         answer = "Ddu1Cy8Tx5Q2rxpWCOMoGmG+GWFLQcJyzz+uaASHDkk"
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "No"
     elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
-        answer = "4"
+        answer = "int(4)"
 
     elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
-        answer = "3"
+        answer = "int(3)"
 
     else: 
         ### you should understand why this else case should be included
