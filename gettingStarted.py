b29df6bc384d0b53e3e4ca14b9a98a76db8b298a
eh3300 <151074955+eh3300@users.noreply.github.com>
2024-02-02 17:42:37 -0500

    Update gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -8,22 +8,22 @@ def welcome_assignment_answers(question):
     if question == "Are encoding and encryption the same? - Yes/No":
         answer = "No"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-        answer = "Yes"
+        answer = "No"
     elif question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
         answer = "pcap"
     elif question == "Is it possible to decode a message without a key? - Yes/No":
         answer = "No"
     elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
-        answer = "Yes"
+        answer = "No"
     elif question == "What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
         answer = "Ddu1Cy8Tx5Q2rxpWCOMoGmG+GWFLQcJyzz+uaASHDkk"
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "No"
     elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
-        answer = "int(4)"
+        answer = "3"
 
     elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
-        answer = "int(3)"
+        answer = "4"
 
     else: 
         ### you should understand why this else case should be included
